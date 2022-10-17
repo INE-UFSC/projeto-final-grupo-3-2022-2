9439f9444db68235c5dd8bf85cd497a9a2a00ca6
Andre Rocco <andre.amaral.rocco@gmail.com>
2022-10-17 11:19:41 -0300

    Atualização de versão

--- prototipo/classeTile.py
+++
@@ -1,9 +0,0 @@
-import pygame
-
-class Tile(pygame.sprite.Sprite):
-    def __init__(self, position, size):
-        super().__init__()
-        
-        self.image = pygame.Surface((size, size)) # Cria uma superfície quadrada com o tamanho size
-        self.image.fill('grey')
-        self.rect = self.image.get_rect(topleft = position)
