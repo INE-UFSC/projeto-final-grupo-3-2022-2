6a1e50edeec6032248f04d0f441c54f6a0c32917
gustavokf2003 <gustavokf2003@gmail.com>
2022-11-03 19:07:21 -0300

    Atualização de versão

---
+++ prototipo/config.py
@@ -0,0 +1,37 @@
+levels = [{
+    'level_number': 1,
+    'level_name': 'Level 1',
+    'tile_map': [
+'XXXXXXXXXXXXXXXXXXXXXXXXX',
+'X                X      X',
+'X                X      X',
+'XXXX             X      X',
+'X                X      X',
+'X        XX      X      X',
+'X        X       X      X',
+'X        X              X',
+'X        X              X',
+'X   P    X              X',
+'XXXXXXXXXXXXXXXXXXXXXXXXX']
+},
+{
+    'level_number': 2,
+    'level_name': 'Level 2',
+    'tile_map': [
+'                         ',
+'                         ',
+'                         ',
+'                         ',
+'                         ',
+'     X             X     ',
+'    X               X    ',
+'    X      X        X    ',
+'     X             X     ',
+' XX         P         XX ',
+'XXXXXXXXXXXXXXXXXXXXXXXXX']
+}]
+
+level_tile_size = 48 # Definir tamanho de cada bloco em pixels
+
+screen_width = 1200
+screen_height = 600
