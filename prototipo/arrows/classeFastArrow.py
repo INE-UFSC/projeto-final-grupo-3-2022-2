650d6bc1c36a108edacdcf3e56a0741d020abc93
Andre Rocco <andre.amaral.rocco@gmail.com>
2022-11-04 23:31:42 -0300

    Criados diferentes tipos de flechas

---
+++ prototipo/arrows/classeFastArrow.py
@@ -0,0 +1,9 @@
+from abstractArrow import Arrow
+from os import path
+
+class FastArrow(Arrow):
+    def __init__(self):
+        IMAGE_PATH = path.join('prototipo', 'arrow.png')
+        MINIMUN_SPEED = 20
+
+        super().__init__(IMAGE_PATH, MINIMUN_SPEED, 5, 0.2)
