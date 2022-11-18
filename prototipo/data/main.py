eb6028a5fb43f9d0aee34e02c103f1dc2d6269a4
Lucas <lucascsoares21@gmail.com>
2022-11-18 10:59:49 -0300

    Criação da classe estática de settings

--- prototipo/data/main.py
+++ prototipo/data/main.py
@@ -1,6 +1,8 @@
 import pygame, sys
 import config
+
 from level.classeLevel import Level
+from Settings import Settings
 
 # Setup geral
 pygame.init()
@@ -15,9 +17,14 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.RESIZABLE
 # Nível
 current_level = config.levels[0]
 level = Level(current_level) # Passa a matriz que representa o nível e a superfície onde o nível será desenhado
+display_surface = level.display_surface # Superfície onde o nível será desenhado
 
-# Status de tela cheia
+# Informações da tela
 fullscreen = False
+
+# Define as configurações globais
+Settings.set_surface_offset(int((screen.get_width() - display_surface.get_width()) / 2),
+                            int((screen.get_height() - display_surface.get_height()) / 2))
 
 while True:
     event_listener = pygame.event.get()
@@ -29,9 +36,13 @@ while True:
             pygame.display.quit()
             screen = pygame.display.set_mode((event.w, event.h), pygame.RESIZABLE)
             pygame.display.init()
+            
+            Settings.set_surface_offset(int((screen.get_width() - display_surface.get_width()) / 2),
+                                        int((screen.get_height() - display_surface.get_height()) / 2))
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_F11:
                 fullscreen = not fullscreen
+                
                 if fullscreen:
                     pygame.display.quit()
                     screen = pygame.display.set_mode(MONITOR_SIZE, pygame.FULLSCREEN)
@@ -40,16 +51,17 @@ while True:
                     pygame.display.quit()
                     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.RESIZABLE)
                     pygame.display.init()
+
+                Settings.set_surface_offset(int((screen.get_width() - display_surface.get_width()) / 2),
+                                            int((screen.get_height() - display_surface.get_height()) / 2))
+
     pygame.display.flip()
 
     """ Conteúdo do jogo """
     
     display_surface = level.run(event_listener) # Executa o nível atual
     
-    x_pos = int((screen.get_width() - display_surface.get_width()) / 2) # Centraliza a superfície do nível na tela
-    y_pos = int((screen.get_height() - display_surface.get_height()) / 2) # Centraliza a superfície do nível na tela
-    
-    screen.blit(display_surface, (x_pos, y_pos)) # Desenha a superfície do nível na tela
+    screen.blit(display_surface, Settings.get_surface_offset()) # Desenha a superfície do nível na tela
     display_surface.fill('black') # Pinta a superfície de preto (para evitar rastros de imagem)
     
     """ Fim do conteúdo do jogo """
