aa983ab65a2341cd82a0246523b26efe86160a30
André Amaral Rocco <andre.amaral.rocco@gmail.com>
2022-11-24 10:17:48 -0300

    Adicionada funcionalidade de seleção de origem

--- prototipo/data/states/classeButton.py
+++ prototipo/data/states/classeButton.py
@@ -11,8 +11,18 @@ class Button():
 
         self.__rect = self.__text.get_rect()
     
-    def render(self, screen: pygame.display, center_position: tuple):
-        self.__rect.center = center_position
+    def render(self, screen: pygame.display, position: tuple, position_origin: str = 'center'):
+        if position_origin == 'topleft':
+            self.__rect.topleft = position
+        elif position_origin == 'topright':
+            self.__rect.topright = position
+        elif position_origin == 'bottomleft':
+            self.__rect.bottomleft = position
+        elif position_origin == 'bottomright':
+            self.__rect.bottomright = position
+        else: # Por padrão renderiza a partir de seu centro
+            self.__rect.center = position
+        
         screen.blit(self.__text, self.__rect)
 
     def check_for_hover(self, mouse_position):
