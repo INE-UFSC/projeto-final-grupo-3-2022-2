153edb1aa0cfd299fa2d828e84aa59dde19b7f1c
gustavokf2003 <gustavokf2003@gmail.com>
2022-11-25 11:12:45 -0300

    Detecção de ações implementada dentro da classe

--- prototipo/data/states/stateLevelSelector.py
+++ prototipo/data/states/stateLevelSelector.py
@@ -14,6 +14,7 @@ class LevelSelector(State):
 
         self.__scoreController = ScoreController()
 
+        self.__actions = {'mouse_left': False}
         self.__assets = Assets()
         self.__background = self.__assets.images['background']
 
@@ -35,15 +36,22 @@ class LevelSelector(State):
                 self.RECORDES.append(Button(self.__assets.fonts_path['text'], 50, (255, 255, 255), str(scores[level][0][1])))
             else:
                 self.RECORDES.append(Button(self.__assets.fonts_path['text'], 50, (255, 255, 255), '--'))
+
+    def update_actions(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            self.__actions['mouse_left'] = True
+        if event.type == pygame.MOUSEBUTTONUP and event.button == 1:
+            self.__actions['mouse_left'] = False
     
-    def update(self, delta_time, actions):
-        if actions['mouse_left']:
+    def update(self, delta_time):
+        if self.__actions['mouse_left']:
             if self.VOLTAR.check_for_hover(Settings.mouse_pos()):
                 self.exit_state()
             
             for i in range(0, len(self.NIVEIS)):
                 if self.NIVEIS[i].check_for_hover(Settings.mouse_pos()):
                     LevelPlaying(self._game).enter_state()
+
 
     def render(self, display_surface):
         background = pygame.transform.smoothscale(self.__background, (self._game.screen_width, self._game.screen_height))
