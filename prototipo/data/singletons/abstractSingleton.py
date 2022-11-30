31affd8ce80834757520e9ef7233060ce6384769
Andre Rocco <andre.amaral.rocco@gmail.com>
2022-11-29 21:49:17 -0300

    Corrigido funcionamento do singleton

--- prototipo/data/singletons/abstractSingleton.py
+++ prototipo/data/singletons/abstractSingleton.py
@@ -1,8 +1,5 @@
 class Singleton(object):
-    _instances = {}
-
-    def __call__(cls, *args, **kwargs):
-        if cls not in cls._instances:
-            instance = super().__call__(*args, **kwargs)
-            cls._instances[cls] = instance
-        return cls._instances[cls]
+  def __new__(cls):
+    if not hasattr(cls, 'instance'):
+      cls.instance = super(Singleton, cls).__new__(cls)
+    return cls.instance
