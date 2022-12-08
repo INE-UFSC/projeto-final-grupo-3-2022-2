5714252e934e743225f22e0687612ce66a67b0f8
Andre Rocco <andre.amaral.rocco@gmail.com>
2022-12-08 19:04:15 -0300

    Removido código obsoleto do LevelDAO

--- prototipo/data/daos/LevelDAO.py
+++ prototipo/data/daos/LevelDAO.py
@@ -2,44 +2,8 @@ from daos.abstractDAO import AbstractDAO
 from daos.exceptions.NivelJaExisteException import NivelJaExisteException
 from daos.exceptions.NivelNaoExisteException import NivelNaoExisteException
 from utility.staticLevelUtility import LevelUtility
-from json import load
 import os
 from utility.finder import find_file
-
-
-# class LevelDAO(AbstractDAO):
-#     def __init__(self, datasource):
-#         super().__init__(cache=[], datasource=datasource)
-
-#     def add_level(self, level_dict: dict):
-#         # Confere o nome do nível no dicionário
-#         if 'level_name' not in level_dict:
-#             raise Exception("A chave 'level_name' contendo o nome do nível não foi encontrada no dicionário.")
-#         elif level_dict['level_name'] == '' or isinstance(level_dict['level_name'], str) == False:
-#             raise Exception("A chave 'level_name' contendo o nome do nível tem um valor vazio ou não é uma string.")
-#         else:
-#             for level in self._objectCache:
-#                 if level['level_name'] == level_dict['level_name']:
-#                     raise NivelJaExisteException(f"Level with name '{level_dict['level_name']}' already exists.")
-
-#         # Tenta fazer a conversão do nível (a validação das informações específicas de criação de nível são conferidas)
-#         try:
-#             converted_dict = LevelUtility.convert(level_dict)
-#         except Exception as e:
-#             raise e
-
-#         self._objectCache.append(converted_dict)
-#         self._dump()
-
-#     def remove_level(self, level_name: str):
-#         for i, level in enumerate(self._objectCache, start = 0):
-#             if level['level_name'] == level_name:
-#                 self._objectCache.pop(i)
-#                 return
-#         raise NivelNaoExisteException(f"Level with name {level_name} doesn't exist.")
-
-
-DEFAULT_LEVELS_PATH = find_file('default-levels.json')
 
 class LevelDAO(AbstractDAO):
     def __init__(self, filename):
